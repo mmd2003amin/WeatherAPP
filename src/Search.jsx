fff834331ad641c98ed7cd358c3c56f781eb5084
Amin Basati <mohamadamin386mm@gmail.com>
2023-08-02 21:22:14 +0330

    Getting the city entered in the search box

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,12 +1,26 @@
 import React from "react";
-import searchIMG from "./assets/search.svg"
+import searchIMG from "./assets/search.svg";
 
-const Search = () => {
+const Search = ({ setName }) => {
+  
+  const searchHandler = (e) => {
+    setName(e.target.value);
+  };
+
   return (
     <div>
       <div className="flex justify-center">
-        <input type="text" placeholder="Search ..." className="mr-4 pl-2 outline-none rounded-3xl h-10 w-full"/>
-        <img src={searchIMG} title="searchIcon" className="bg-white rounded-full w-10 p-1.5" />
+        <input
+          type="text"
+          placeholder="Search ..."
+          className="mr-4 pl-2 outline-none rounded-3xl h-10 w-full"
+          onChange={searchHandler}
+        />
+        <img
+          src={searchIMG}
+          title="searchIcon"
+          className="bg-white rounded-full w-10 p-1.5"
+        />
       </div>
     </div>
   );
--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "./Search";
 
 const Weather = () => {
+  const [name, setName] = useState("");
+
   return (
     <div className="cart">
-      <Search />
+      <Search setName={setName} />
     </div>
   );
 };
